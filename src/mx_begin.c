3e650b7dc71f62071df5cf0008e89be34af008bf
leodobrovolsky <leodobrovolsky.com@gmail.com>
2020-01-06 16:06:20 +0200

    mx_begin.c
    comments

--- src/mx_begin.c
+++ src/mx_begin.c
@@ -35,21 +35,23 @@ static bool check_symbol(char *print_name) {
 
 void mx_begin(char **files_name, char *opt) {
     int file_n = size_arr(files_name);
-    t_file **files = create_struct_arr(file_n);
-
+    t_file **files = create_struct_arr(file_n); //массив структур файлов
+    
     for (int i = 0; files_name[i]; i++)
-        files[i] = mx_get_lstat(files_name[i]);
+        files[i] = mx_get_lstat(files_name[i]); //заполнение массива структурами
 
-    mx_print_ls(files, file_n, opt);
+    //здесь нужно вставить функцию сортировки
+    
+    mx_print_ls(files, file_n, opt); //печать файлов
 
     for (int i = 0; files[i]; i++)
-        if (files[i]->type == 'd' && mx_get_char_index(opt, 'R') >= 0) {
+        if (files[i]->type == 'd' && mx_get_char_index(opt, 'R') >= 0) { //если файл - директория и есть флаг R
             if (mx_get_char_index(opt, 'a') >= 0 
-                && !check_symbol(files[i]->print_name))
+                && !check_symbol(files[i]->print_name)) //проверка на . ..
                     continue;
             mx_printstr("\n");
-            mx_printstr(files[i]->name);
+            mx_printstr(files[i]->name); //печатает имя директории
             mx_printstr(":\n");
-            mx_begin(mx_read_dir(files[i]->name, get_hidden(opt)), opt);
+            mx_begin(mx_read_dir(files[i]->name, get_hidden(opt)), opt); // печатает содержимое директории
         }
 }
