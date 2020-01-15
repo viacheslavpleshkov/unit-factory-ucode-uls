c163c7a1f4b1e44f16492317be4649f32e0135b1
Leonid Dobrovolskyi <ldobrovols@e2r5p12.unit.ua>
2020-01-15 21:31:13 +0200

    123

--- src/mx_ls.c
+++ src/mx_ls.c
@@ -19,28 +19,36 @@ static t_ls **create_struct_arr(int files_number) {
 }
 
 void mx_ls(char **str_arr, char *flags) {
-    //char *new_flags = mx_clear_flags(char *flags);
-    // int str_size = mx_get_size_arr(str_arr);
-    // t_ls **files = create_struct_arr(str_size);
-    // char **files_without_dir = mx_until_create_char_arr(str_size);
-    // int k = 0;
+    // char *new_flags = mx_clear_flags(char *flags);
+    int str_size = mx_get_size_arr(str_arr);
+    t_ls **files = create_struct_arr(str_size);
+    char **files_without_dir = mx_until_create_char_arr(str_size);
+    int k = 0;
 
-    // if (!str_size)
-    //     mx_ls_loop(mx_read_dir(".", get_hidden(flags)), flags);
+    if (!str_size)
+        mx_ls_loop(mx_read_dir(".", get_hidden(flags)), flags);
     
-    // for (int i = 0; i < str_size; i++) {
-    //     files[k] = mx_get_lstat(str_arr[i]);
-    //     if (files[k]->type == 'd')
-    //         free(files[k]);
-    //     else
-    //         k++;
-    //     mx_print_lstat(files[i]);
-    // }
+    for (int i = 0; i < str_size; i++)
+        files[i] = mx_get_lstat(str_arr[i]);
+    files[str_size] = NULL;
     
-    // for (int i = 0; files[i]; i++)
-    //     files_without_dir[i] = mx_strdup(files[i]->name);
+    for (int i = 0; files[i]; i++) {
+        files_without_dir[k] = mx_strdup(files[i]->name);
+        if (files[i]-> type != 'd')
+            k++;
+        else
+            mx_strdel(&files_without_dir[k]);
+    }
     
-    // mx_ls_loop(files_without_dir, flags);
+    mx_ls_loop(files_without_dir, flags);
     
-    mx_ls_loop(str_arr, flags);
+    for (int i = 0; files[i]; i++)
+        if (files[i]->type == 'd') {
+            mx_printstr(files[i]->print_name);
+            mx_printstr(":\n");
+            mx_ls_loop(mx_read_dir(files[i]->name, get_hidden(flags)), flags);
+        }
+
+
+    //mx_ls_loop(str_arr, flags);
 }
