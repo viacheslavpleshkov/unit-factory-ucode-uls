84d275fd5906ab1ede84385a4c2e30d7471f2a16
Slava Pleshkov <mail@slavapleshkov.com>
2020-01-15 23:20:54 +0200

    Upgrade

--- src/mx_ls.c
+++ src/mx_ls.c
@@ -1,32 +1,14 @@
 #include "uls.h"
 
-static int get_hidden(char *flags) {
-    if (mx_get_char_index(flags, 'a') >= 0)
-        return LS_HIDDEN_a;
-    else if (mx_get_char_index(flags, 'A') >= 0)
-        return LS_HIDDEN_A;
-    else
-        return LS_HIDDEN_NOT;
-}
-
-static t_ls **create_struct_arr(int files_number) {
-    t_ls **head = malloc(sizeof(t_ls *) * (files_number + 1));
-
-    for (int i = 0; i < files_number; i++)
-        head[i] = malloc(sizeof(t_ls));
-    head[files_number] = NULL;
-    return head;
-}
-
 void mx_ls(char **str_arr, char *flags) {
     // char *new_flags = mx_clear_flags(char *flags);
     int str_size = mx_get_size_arr(str_arr);
-    t_ls **files = create_struct_arr(str_size);
+    t_ls **files = mx_ls_create_struct_arr(str_size);
     char **files_without_dir = mx_until_create_char_arr(str_size);
     int k = 0;
 
     if (!str_size)
-        mx_ls_loop(mx_read_dir(".", get_hidden(flags)), flags);
+        mx_ls_loop(mx_read_dir(".", mx_ls_get_hidden(flags)), flags);
     
     for (int i = 0; i < str_size; i++)
         files[i] = mx_get_lstat(str_arr[i]);
@@ -46,9 +28,6 @@ void mx_ls(char **str_arr, char *flags) {
         if (files[i]->type == 'd') {
             mx_printstr(files[i]->print_name);
             mx_printstr(":\n");
-            mx_ls_loop(mx_read_dir(files[i]->name, get_hidden(flags)), flags);
+            mx_ls_loop(mx_read_dir(files[i]->name, mx_ls_get_hidden(flags)), flags);
         }
-
-
-    //mx_ls_loop(str_arr, flags);
 }
