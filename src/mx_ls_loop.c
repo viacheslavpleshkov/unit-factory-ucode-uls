fc23b81dc247b8b4b67ae3ddc6ddddc067a959a2
Viacheslav Pleshkov <work@viacheslavpleshkov.com>
2020-01-23 15:08:21 +0200

    Add flag -t uls

--- src/mx_ls_loop.c
+++ src/mx_ls_loop.c
@@ -33,18 +33,7 @@ void mx_ls_loop(char **files_name, char *flags) {
         files[i] = mx_get_lstat(files_name[i]); //заполнение массива структурами
 
     //здесь нужно вставить функцию сортировки
-    mx_ls_sort_default(files);
-    if (mx_get_char_index(flags, 'r') >= 0)
-        mx_ls_sort_flag_r(files);
-    if (mx_get_char_index(flags, 't') >= 0)
-        mx_ls_sort_flag_t(files);
-    if (mx_get_char_index(flags, 'u') >= 0)
-        mx_ls_sort_flag_u(files);
-    if (mx_get_char_index(flags, 'c') >= 0)
-        mx_ls_sort_flag_c(files);
-    if (mx_get_char_index(flags, 'S') >= 0)
-        mx_ls_sort_flag_big_s(files);
-    
+    mx_ls_sort(files, flags);
     mx_ls_print(files, file_n, flags); //печать файлов
 
     for (int i = 0; files[i]; i++)
--- src/mx_ls_print_l.c
+++ src/mx_ls_print_l.c
@@ -64,3 +64,41 @@ void mx_ls_print_l(t_ls **files, int file_n, char *opt) {
     }
     opt = NULL;
 }
+
+void mx_ls_print_big_t(t_ls **files, int file_n, char *opt) {
+    int max_nlink_len = get_max_nlink(files);
+    int max_size_len = get_max_size(files);
+    char *time_str = NULL;
+    char *temp = NULL;
+    long long int total = 0;
+ 
+    mx_printstr("total ");  
+    for (int i = 0; i < file_n; i++)
+        total += files[i]->blocks;
+    mx_printstr(mx_itoa(total));
+    mx_printchar('\n');
+
+    for (int i = 0; i < file_n; i++) {
+        temp = ctime(&files[i]->mtime);
+        time_str = mx_strndup(&temp[4], 20);
+        mx_printchar(files[i]->type);
+        mx_printstr(mx_ls_get_rwx_str(files[i]->mode));
+        mx_printstr(" ");
+        mx_printstr(" ");
+        mx_until_print_format_str(mx_itoa((int)files[i]->nlink), 'r', ' ', max_nlink_len);
+        mx_printstr(" ");
+        mx_printstr(files[i]->uid_name);
+        mx_printstr("  ");
+        mx_printstr(files[i]->gid_name);
+        mx_printstr("  ");
+        mx_until_print_format_str(mx_itoa((int)files[i]->size), 'r', ' ', max_size_len);
+        mx_printstr(" ");
+        mx_printstr(time_str);
+        mx_printstr(" ");
+        mx_printstr(files[i]->print_name);
+        mx_printstr("\n");
+        // mx_strdel(&temp);
+        // mx_strdel(&time_str);
+    }
+    opt = NULL;
+}
