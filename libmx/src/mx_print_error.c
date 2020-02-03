b189eeb66339b27e12ea52d0e49804235576240a
Leonid Dobrovolskyi <ldobrovols@e2r5p3.unit.ua>
2020-02-03 18:45:14 +0200

    libmx

--- libmx/src/mx_print_error.c
+++ libmx/src/mx_print_error.c
@@ -1,7 +1,8 @@
 #include "libmx.h"
 
-void mx_print_error(const char *str, int exit_code) {
+void mx_print_error(const char *str, int exit_code, bool exit_status) {
     write(2, str, mx_strlen(str));
     write(2, "\n", 1);
-    exit(exit_code);
+    if (exit_status)
+        exit(exit_code);
 }
