4303e84b3a472488054a9ff9100f51d8a04567c7
Viacheslav Pleshkov <work@viacheslavpleshkov.com>
2020-02-08 14:26:00 +0200

    Upgrade files

--- src/mx_ls_print_one.c
+++ src/mx_ls_print_one.c
@@ -2,10 +2,7 @@
 
 void mx_ls_print_one(t_ls **files, t_main *main) {
     for (int i = 0; i != main->file_n; i++) {
-        if (i != 0)
-            mx_printstr("\n");
         mx_ls_print_color(files[i]->color, files[i]->print_name, main->color);
+        mx_printstr("\n");
     }
-    mx_printstr("\n");
-
 }
