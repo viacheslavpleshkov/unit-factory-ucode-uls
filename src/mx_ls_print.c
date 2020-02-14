43e6437ac2f53cca9a6407741490124ba43ec951
Viacheslav Pleshkov <work@viacheslavpleshkov.com>
2020-02-14 22:25:07 +0200

    Update error

--- src/mx_ls_print.c
+++ src/mx_ls_print.c
@@ -1,26 +1,19 @@
 #include "uls.h"
 
 void mx_ls_print(t_ls **files, t_main *main) {
-    char *flags = main->flags;
-    char *name = NULL;
-    
-    if (mx_untill_get_max_size(files) == 1 && !(files[0]->type == 'l')) {
-        name = mx_strdup(files[0]->print_name);
-        mx_ls_error(ERR_DIR, name);
-        mx_strdel(&name);
-    } else {
-        if (mx_ls_check_flag(flags, 'l'))
-            mx_ls_print_l(files, main);
-        else if (mx_ls_check_flag(flags, '1'))
-            mx_ls_print_one(files, main);
-        else if (mx_ls_check_flag(flags, 'm'))
-            mx_ls_print_m(files, main);
-        else if (main->out_in_terminal == false)
-            if (mx_ls_check_flag(flags, 'C'))
-                mx_ls_print_big_c(files, main);
-            else
-                mx_ls_print_one(files, main);
-        else
+    char *flags = main->flags;    
+
+    if (mx_ls_check_flag(flags, 'l'))
+        mx_ls_print_l(files, main);
+    else if (mx_ls_check_flag(flags, '1'))
+        mx_ls_print_one(files, main);
+    else if (mx_ls_check_flag(flags, 'm'))
+        mx_ls_print_m(files, main);
+    else if (main->out_in_terminal == false)
+        if (mx_ls_check_flag(flags, 'C'))
             mx_ls_print_big_c(files, main);
-    }
+        else
+            mx_ls_print_one(files, main);
+    else
+        mx_ls_print_big_c(files, main);
 }
