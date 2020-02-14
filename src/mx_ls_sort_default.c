10d50cb0461b3619240a696a061b8255b9f37fb8
Viacheslav Pleshkov <work@viacheslavpleshkov.com>
2020-02-14 20:52:30 +0200

    Update

--- src/mx_ls_sort_default.c
+++ src/mx_ls_sort_default.c
@@ -5,7 +5,7 @@ void mx_ls_sort_default(t_ls **arr, int size) {
 
     for (int i = 0; i < size; ++i) {
         for (int j = i; j < size; ++j) {
-            if (mx_strcmp(arr[i]->name, arr[j]->name) > 0) {
+            if (mx_strcmp(arr[i]->print_name, arr[j]->print_name) > 0) {
                 temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
