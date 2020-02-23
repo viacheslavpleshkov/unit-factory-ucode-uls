d33b80b54868091c957720df93e0ae30f51c49e6
Viacheslav Pleshkov <work@viacheslavpleshkov.com>
2020-02-23 22:59:05 +0200

    Fix print

--- src/utils/mx_until_print_format_str.c
+++ src/utils/mx_until_print_format_str.c
@@ -9,14 +9,14 @@ void mx_until_print_format_str(char *str, char local, char symbol, int size) {
     int len = mx_strlen(str);
 
     if (local == 'c')
-        print_symbol(' ', (size - len) / 2); 
+        print_symbol(symbol, (size - len) / 2); 
     if (local == 'r')
-        print_symbol(' ', (size - len) / 2); 
+        print_symbol(symbol, size - len); 
     mx_printstr(str);
     if (local == 'c')
-        print_symbol(' ', size - len - (size - len) / 2);
+        print_symbol(symbol, size - len - (size - len) / 2);
     if (local == 'l')
-        print_symbol(' ', size - len);
+        print_symbol(symbol, size - len);
 }
 
 void mx_until_print_format_l(t_ls *file, t_main *main, int size) {
@@ -26,7 +26,7 @@ void mx_until_print_format_l(t_ls *file, t_main *main, int size) {
     if (name == 'c')
         print_symbol(' ', (size - len) / 2); 
     if (name == 'r')
-        print_symbol(' ', (size - len) / 2); 
+        print_symbol(' ', size - len); 
     mx_ls_print_color(file, main->color);
     if (name == 'c')
         print_symbol(' ', size - len - (size - len) / 2);
